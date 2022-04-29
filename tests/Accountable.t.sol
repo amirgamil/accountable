012dabcdd962b78040a7ab9f009a21d0f984a163
amirgamil <amir.amgad01@gmail.com>
2022-04-28 23:28:03 -0700

    update favicons and forge tests

--- tests/Accountable.t.sol
+++ tests/Accountable.t.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.10;
 import "forge-std/Test.sol";
 import "../contracts/Accountable.sol";
 
+//Note Foundry tests are NOT complete because it does not play well with call.
+//I did significantly more complete tests via a local hardhat node
 contract ContractTest is Test{
     Accountable accountable;
 
@@ -22,37 +24,58 @@ contract ContractTest is Test{
         accountable.createNewStake{value: 0 ether}("test", address(1));
     }
 
-    // function testFuzzStakeCreation(uint256 val) public {
-    //     vm.assume(val != 0);
-    //     accountable.createNewStake{value: val }("test", address(1));
-    //     Accountable.Stake[] memory allStakes = accountable.getAllStakes();
-    //     assertTrue(allStakes.length == 1);
-    //     assertEq(allStakes[0].name, "test");
-    // }
-
-    function testSuccessfulStakeCompletion() public {
+    function testSuccessfulConfirmStake() public {
         address currentStakee = address(this);
-        //seed contract with some funds first
         vm.deal(currentStakee, 1 ether);
 
-        console.log("Hello: ", address(this).balance);
-
-        accountable.createNewStake{value: 0.1 ether}("test", address(1));
+        accountable.createNewStake{value: 1 ether}("test", address(2));
         Accountable.Stake[] memory allStakes = accountable.getAllStakes();
         
         assertTrue(allStakes.length == 1);
         assertEq(allStakes[0].name, "test"); 
+        assertTrue(allStakes[0].status == Accountable.Status.Unconfirmed);
 
-        vm.startPrank(address(1));
-        accountable.markStakeSuccessful(allStakes[0].id);
-        vm.stopPrank();
+        vm.startPrank(address(2));
+        accountable.confirmStakeWithBuddy(0);
 
         Accountable.Stake[] memory updatedStakes = accountable.getAllStakes();
-        assertTrue(updatedStakes[0].status == Accountable.Status.Success);
+        assertTrue(updatedStakes[0].status == Accountable.Status.Pending);
+
     }
 
-    function testCheeky() public {
-        console.log(address(this));
+    function testFailStrangerConfirmStake() public {
+        address currentStakee = address(this);
+        vm.deal(currentStakee, 1 ether);
+
+        accountable.createNewStake{value: 1 ether}("test", address(2));
+        Accountable.Stake[] memory allStakes = accountable.getAllStakes();
+        
+        assertTrue(allStakes.length == 1);
+        assertEq(allStakes[0].name, "test"); 
+        assertTrue(allStakes[0].status == Accountable.Status.Unconfirmed);
+
+        vm.startPrank(address(4));
+        accountable.confirmStakeWithBuddy(0);
+
+        Accountable.Stake[] memory updatedStakes = accountable.getAllStakes();
+        assertTrue(updatedStakes[0].status == Accountable.Status.Pending);
+    }
+
+
+    function testFailStrangerAbortStake() public {
+        address currentStakee = address(this);
+        vm.deal(currentStakee, 1 ether);
+
+        accountable.createNewStake{value: 1 ether}("test", address(2));
+        Accountable.Stake[] memory allStakes = accountable.getAllStakes();
+        
+        assertTrue(allStakes.length == 1);
+        assertEq(allStakes[0].name, "test"); 
+        assertTrue(allStakes[0].status == Accountable.Status.Unconfirmed);
+
+        vm.startPrank(address(4));
+        accountable.widthrawMoneyBeforeConfirmation(0);
+
     }
 
 }
